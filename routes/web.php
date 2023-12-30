d2ab306544db0cdae7ea2e9c42ab820c14732213
Mohamed13796 <mohamedyasser13796@gmail.com>
2023-12-30 21:01:36 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,89 @@
+<?php
+
+use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\BrandsController;
+use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\HomeController;
+use App\Http\Controllers\admin\SubCategoryController;
+use App\Http\Controllers\admin\TempImagesController;
+use App\Models\SubCategory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin/login',[AdminLoginController::class,'index'])->name('admin.login');
+
+Route::group(['prefix'=>'admin'],function(){
+
+    Route::group(['middleware'=>'admin.guest'],function(){
+        Route::get('/login',[AdminLoginController::class,'index'])->name('admin.login');
+        Route::post('/authenticate',[AdminLoginController::class,'authenticate'])->name('admin.authenticate');
+
+    });
+
+    Route::group(['middleware'=>'admin.auth'],function(){
+     
+        Route::get('/dashboard',[HomeController::class,'index'])->name('admin.dashboard');
+        Route::get('/logout',[HomeController::class,'logout'])->name('admin.logout');
+
+        // Caregory Routes
+        Route::get('/categories',[CategoryController::class,'index'])->name('categories.index');
+        Route::get('/categories/create',[CategoryController::class,'create'])->name('categories.create');
+        Route::post('/categories',[CategoryController::class,'store'])->name('categories.store');
+        Route::get('/categories/{category}/edit',[CategoryController::class,'edit'])->name('categories.edit');
+        Route::put('/categories/{category}',[CategoryController::class,'update'])->name('categories.update');
+        Route::delete('/categories/{category}',[CategoryController::class,'delete'])->name('categorie.delete');
+        
+        
+        // Sub Category Routes
+        Route::get('/sub-categories',[SubCategoryController::class,'index'])->name('sub-categories.index');
+        Route::get('/sub-categories/create',[SubCategoryController::class,'create'])->name('sub-categories.create');
+        Route::post('/sub-categories',[SubCategoryController::class,'store'])->name('sub-categories.store');
+        Route::get('/sub-categories/{subcategory}/edit',[SubCategoryController::class,'edit'])->name('sub-categories.edit');
+        Route::put('/sub-categories/{subcategory}',[SubCategoryController::class,'update'])->name('sub-categories.update');
+        Route::delete('/sub-categories/{subcategory}',[SubCategoryController::class,'delete'])->name('sub-categorie.delete');
+        
+        
+        //Brands Routes
+        Route::get('/brands',[BrandsController::class,'index'])->name('brands.index');
+        Route::get('/brands/create',[BrandsController::class,'create'])->name('brands.create');
+        Route::post('/brands',[BrandsController::class,'store'])->name('brands.store');
+        Route::get('/brands/{brand}/edit',[BrandsController::class,'edit'])->name('brands.edit');
+        Route::put('/brands/{brand}',[BrandsController::class,'update'])->name('brands.update');
+        Route::delete('/brands/{brand}',[BrandsController::class,'delete'])->name('brands.delete');
+        
+
+        //temp-images.create
+        Route::post('/upload-temp-image',[TempImagesController::class,'create'])->name('temp-images.create');
+       
+        
+        Route::get('/getSlug',function(Request $request){
+            $slug = '';
+            if(!empty($request->title)){
+                $slug = Str::slug($request->title);
+            }
+            return response()->json([
+                'status' => true,
+                'slug' => $slug
+            ]);
+        })->name('getSlug');
+
+    });
+
+
+
+});
